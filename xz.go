0444ccccc34ce304233ac9021e0aa53a107fbe88
branjevectn <3105729+branjevectn@users.noreply.github.com>
2024-08-12 15:17:00 +0500

    Update xz.go

--- xz.go
+++ xz.go
@@ -4,7 +4,7 @@ import ("fmt")
 func main() {
   var (
     sum1 = 10205 + 5408 // 150 (100 + 50)
-    sum2 = sum1 + 29250 // 400 (150 + 250)
+    sum2 = sum1 + 2750 // 400 (150 + 250)
     sum3 = sum2 + sum2 // 800 (400 + 400)
   )
   fmt.Println(sum3)
